1edfc58e1a2576128644fccc8c329387c8632319
Outlik <jgchkcu@gmail.com>
2026-09-03 00:58:38 +0700

    Add product catalog data

---
+++ assets/catalog.js
@@ -0,0 +1,301 @@
+window.OPERA_CATALOG=[
+  {
+    "category": "Рекламная продукция",
+    "slug": "pechat-vizitok",
+    "title": "Визитки",
+    "short": "Визитки от 1 экземпляра: классические, на дизайнерской бумаге, пластике и с декоративной отделкой.",
+    "productKey": "businessCards",
+    "tags": [
+      "от 1 шт.",
+      "дизайнерская бумага",
+      "пластик"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-vizitok/01.jpg"
+  },
+  {
+    "category": "Рекламная продукция",
+    "slug": "pechat-listovok",
+    "title": "Листовки",
+    "short": "Оперативная цифровая печать листовок разных форматов и тиражей.",
+    "productKey": "leaflets",
+    "tags": [
+      "A6–A3",
+      "от 1 шт.",
+      "4+0 / 4+4"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-listovok/01.jpg"
+  },
+  {
+    "category": "Рекламная продукция",
+    "slug": "pechat-plakatov",
+    "title": "Плакаты",
+    "short": "Плакаты для событий, промо и информационных материалов.",
+    "productKey": "posters",
+    "tags": [
+      "срочная печать",
+      "A3 и меньше",
+      "ламинация"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-plakatov/01.jpg"
+  },
+  {
+    "category": "Рекламная продукция",
+    "slug": "pechat-flaerov",
+    "title": "Флаеры",
+    "short": "Компактный рекламный формат для раздачи, промо и купонов.",
+    "productKey": "leaflets",
+    "tags": [
+      "от 1 шт.",
+      "быстро",
+      "двусторонняя печать"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-flaerov/01.jpg"
+  },
+  {
+    "category": "Рекламная продукция",
+    "slug": "pechat-otkrytok",
+    "title": "Открытки",
+    "short": "Печатные открытки на обычных и дизайнерских материалах.",
+    "productKey": "postcards",
+    "tags": [
+      "плотные бумаги",
+      "тиснение",
+      "ламинация"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-otkrytok/01.jpg"
+  },
+  {
+    "category": "Рекламная продукция",
+    "slug": "pechat-priglashenii",
+    "title": "Приглашения",
+    "short": "Приглашения для мероприятий, компаний и частных событий.",
+    "productKey": "postcards",
+    "tags": [
+      "дизайнерская бумага",
+      "от 1 шт.",
+      "декоративная отделка"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-priglashenii/01.jpg"
+  },
+  {
+    "category": "Рекламная продукция",
+    "slug": "pechat-diplomov-i-gramot",
+    "title": "Дипломы и грамоты",
+    "short": "Печать дипломов, грамот и наградных материалов.",
+    "productKey": "certificates",
+    "tags": [
+      "персонализация",
+      "нумерация",
+      "плотные бумаги"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-diplomov-i-gramot/01.jpg"
+  },
+  {
+    "category": "Рекламная продукция",
+    "slug": "pechat-sertifikatov",
+    "title": "Сертификаты",
+    "short": "Подарочные, именные и корпоративные сертификаты.",
+    "productKey": "certificates",
+    "tags": [
+      "от 1 шт.",
+      "переменные данные",
+      "тиснение"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-sertifikatov/01.jpg"
+  },
+  {
+    "category": "Презентационная продукция",
+    "slug": "pechat-katalogov",
+    "title": "Каталоги",
+    "short": "Многостраничные каталоги продукции и услуг.",
+    "productKey": "catalogs",
+    "tags": [
+      "многостраничные",
+      "скрепка / пружина",
+      "от 1 шт."
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-katalogov/01.jpg"
+  },
+  {
+    "category": "Презентационная продукция",
+    "slug": "pechat-bukletov",
+    "title": "Буклеты",
+    "short": "Буклеты с одним или несколькими сгибами.",
+    "productKey": "booklets",
+    "tags": [
+      "фальцовка",
+      "биговка",
+      "A4 / A3"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-bukletov/01.jpg"
+  },
+  {
+    "category": "Презентационная продукция",
+    "slug": "pechat-broshiur",
+    "title": "Брошюры",
+    "short": "Компактные многостраничные издания для презентаций и рекламы.",
+    "productKey": "catalogs",
+    "tags": [
+      "от 1 шт.",
+      "многостраничные",
+      "сборка тиража"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-broshiur/01.jpg"
+  },
+  {
+    "category": "Презентационная продукция",
+    "slug": "pechat-blankov",
+    "title": "Бланки",
+    "short": "Фирменные бланки для деловой документации.",
+    "productKey": "forms",
+    "tags": [
+      "A4 / A5",
+      "офсетная бумага",
+      "персонализация"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-blankov/01.jpg"
+  },
+  {
+    "category": "Презентационная продукция",
+    "slug": "pechat-konvertov",
+    "title": "Конверты",
+    "short": "Фирменные и информационные конверты с печатью.",
+    "productKey": "forms",
+    "tags": [
+      "фирменный стиль",
+      "малые тиражи",
+      "цифровая печать"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-konvertov/01.jpg"
+  },
+  {
+    "category": "Презентационная продукция",
+    "slug": "pechat-papok",
+    "title": "Папки",
+    "short": "Фирменные папки из плотного картона.",
+    "productKey": "folders",
+    "tags": [
+      "плотный картон",
+      "биговка",
+      "фигурная резка"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-papok/01.jpg"
+  },
+  {
+    "category": "Презентационная продукция",
+    "slug": "pechat-kubarikov",
+    "title": "Кубарики",
+    "short": "Блоки для заметок и фирменные кубарики.",
+    "productKey": "notepads",
+    "tags": [
+      "офисная полиграфия",
+      "фирменная печать",
+      "блоки для заметок"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-kubarikov/01.jpg"
+  },
+  {
+    "category": "В помощь бизнесу",
+    "slug": "pechat-meniu",
+    "title": "Меню",
+    "short": "Меню для кафе, ресторанов и баров с износостойкой обработкой.",
+    "productKey": "menus",
+    "tags": [
+      "ламинация",
+      "пружина / болты",
+      "плотные материалы"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-meniu/01.jpg"
+  },
+  {
+    "category": "В помощь бизнесу",
+    "slug": "pechat-voblerov-i-birok",
+    "title": "Бирки и воблеры",
+    "short": "Бирки, воблеры и POS-материалы нестандартной формы.",
+    "productKey": "tags",
+    "tags": [
+      "фигурная резка",
+      "отверстия",
+      "пластик / картон"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-voblerov-i-birok/01.jpg"
+  },
+  {
+    "category": "В помощь бизнесу",
+    "slug": "pechat-teibl-tentov",
+    "title": "Тейбл-тенты",
+    "short": "Настольные тейбл-тенты для заведений и точек продаж.",
+    "productKey": "menus",
+    "tags": [
+      "биговка",
+      "ламинация",
+      "кафе / рестораны"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-teibl-tentov/01.jpg"
+  },
+  {
+    "category": "В помощь бизнесу",
+    "slug": "pechat-etiketok",
+    "title": "Этикетки",
+    "short": "Этикетки для упаковки, маркировки и продукции.",
+    "productKey": "stickers",
+    "tags": [
+      "самоклейка",
+      "контурная резка",
+      "малые тиражи"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-etiketok/01.jpg"
+  },
+  {
+    "category": "В помощь бизнесу",
+    "slug": "pechat-nakleek-i-stikerov",
+    "title": "Наклейки и стикеры",
+    "short": "Наклейки на самоклеящейся бумаге и плёнке от 1 экземпляра.",
+    "productKey": "stickers",
+    "tags": [
+      "бумага / плёнка",
+      "от 1 шт.",
+      "плоттерная резка"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-nakleek-i-stikerov/01.jpg"
+  },
+  {
+    "category": "Многостраничные издания",
+    "slug": "pechat-knig-fotoknig",
+    "title": "Книги",
+    "short": "Печать книг малыми тиражами и от 1 экземпляра.",
+    "productKey": "books",
+    "tags": [
+      "от 1 шт.",
+      "твёрдый переплёт",
+      "мягкий переплёт"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-knig-fotoknig/01.jpg"
+  },
+  {
+    "category": "Многостраничные издания",
+    "slug": "pechat-knig-fotoknig-foto",
+    "title": "Фотоальбомы",
+    "short": "Фотоальбомы и фотокниги с качественной цветной печатью.",
+    "productKey": "books",
+    "tags": [
+      "фотокниги",
+      "от 1 шт.",
+      "цветная печать"
+    ],
+    "image": "https://www.operaprint.ru/data/pechat-knig-fotoknig/01.jpg"
+  },
+  {
+    "category": "Многостраничные издания",
+    "slug": "proizvodstvo-kalendarei",
+    "title": "Календари",
+    "short": "Настенные, настольные и карманные календари.",
+    "productKey": "calendars",
+    "tags": [
+      "настенные",
+      "настольные",
+      "пружина"
+    ],
+    "image": "https://www.operaprint.ru/data/proizvodstvo-kalendarei/01.jpg"
+  }
+];
